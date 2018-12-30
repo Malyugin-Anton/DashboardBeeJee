69a75e6c950dd94b78d1eeb7bf9729bc156d4d5a
Malyugin-Anton <ya.anton.m22@gmail.com>
2018-12-30 14:42:38 +0400

    add table

--- src/App.js
+++ src/App.js
@@ -2,13 +2,46 @@ import React from 'react';
 import {
   connect
 } from 'react-redux'
+import {
+  Table,
+  Divider,
+  Icon,
+  Tag
+} from 'antd'
+
+const columns = [{
+  title: 'ID',
+  dataIndex: 'id',
+  key: 'id'
+}, {
+  title: 'username',
+  dataIndex: 'username',
+  key: 'username',
+}, {
+  title: 'email',
+  dataIndex: 'email',
+  key: 'email',
+  render: (email) => {
+    return <Tag color="blue" key={email}>{email}</Tag>
+  }
+}, {
+  title: 'text',
+  key: 'text',
+  dataIndex: 'text'
+}, {
+  title: 'status',
+  key: 'status',
+  dataIndex: 'status',
+  render: (status) => (
+    ( status ) ? <Icon type="question-circle" theme="twoTone" /> : <Icon type="check-circle" theme="twoTone" />
+  ),
+}];
 
 const App = ({
     tasks
   }) => {
-  console.log(tasks);
   return (
-    <h1> Start </h1>
+    <Table columns={columns} dataSource={tasks} />
   )
 }
 
