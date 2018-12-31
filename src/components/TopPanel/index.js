33784fd6bb806ce095532f70e68de8a9c9a32592
Malyugin-Anton <ya.anton.m22@gmail.com>
2018-12-31 12:39:46 +0400

    bug fix

---
+++ src/components/TopPanel/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import {
+  Button
+} from 'antd';
+
+const TopPanel = () => {
+
+  return (
+    <div className="top-panel">
+      <Button type="primary">
+        LOGIN
+      </Button>
+    </div>
+  )
+
+}
+
+export default TopPanel;
