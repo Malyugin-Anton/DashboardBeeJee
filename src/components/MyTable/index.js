e231a1593418f8a5b1230e895f4f94f3947b22b8
Malyugin-Anton <ya.anton.m22@gmail.com>
2019-01-02 01:54:13 +0400

    current action edit and save btn clock

--- src/components/MyTable/index.js
+++ src/components/MyTable/index.js
@@ -4,13 +4,75 @@ import {
   Table,
   Icon,
   Tag,
-  Button
+  Input,
+  InputNumber,
+  Button,
+  Form,
+  Switch
 } from 'antd'
+
+
+const FormItem = Form.Item;
+const EditableContext = React.createContext();
+
+const EditableRow = ({ form, index, ...props }) => (
+  <EditableContext.Provider value={form}>
+    <tr {...props} />
+  </EditableContext.Provider>
+);
+
+const EditableFormRow = Form.create()(EditableRow);
+
+class EditableCell extends React.Component {
+  getInput = (record) => {
+    if (this.props.dataIndex === 'status') {
+      return (record.status) ? <Switch /> : <Switch defaultChecked/>;
+    }
+    return <Input / > ;
+  };
+
+  render() {
+    const {
+      editing,
+      dataIndex,
+      title,
+      inputType,
+      record,
+      index,
+      ...restProps
+    } = this.props;
+
+    return (
+      <EditableContext.Consumer>
+        {(form) => {
+          const { getFieldDecorator } = form;
+          return (
+            <td {...restProps}>
+              {editing ? (
+                <FormItem style={{ margin: 0 }}>
+                  {getFieldDecorator(dataIndex, {
+                    rules: [{
+                      required: true,
+                      message: `Please Input ${title}!`,
+                    }],
+                    initialValue: record[dataIndex],
+                  })(this.getInput(record))}
+                </FormItem>
+              ) : restProps.children}
+            </td>
+          );
+        }}
+      </EditableContext.Consumer>
+    )
+  }
+}
 
 class MyTable extends React.Component {
 
   state = {
     edit: false,
+
+    editingKey: '',
 
     columns: [{
       title: 'ID',
@@ -30,42 +92,102 @@ class MyTable extends React.Component {
     }, {
       title: 'text',
       key: 'text',
-      dataIndex: 'text'
+      dataIndex: 'text',
+      editable: true
     }, {
       title: 'status',
       key: 'status',
       dataIndex: 'status',
+      editable: true,
       render: (status) => (
         ( status ) ? <Icon type="question-circle" theme="twoTone" /> : <Icon type="check-circle" theme="twoTone" />
       ),
     }, {
-      title: 'action',
-      dataIndex: 'action',
-      className: this.props.login ? "show" : "hide",
-      render: () => {
-        return this.state.edit 
-          ? <Button onClick={this.handleSave}>Save</Button>
-          : <Button onClick={this.handleEdit}>Edit</Button> 
-      }
+        title: 'action',
+        dataIndex: 'action',
+        className: this.props.login ? "show" : "hide",
+        render: (text, record, index) => {
+          return this.state.edit
+            ? <Button onClick={() => this.handleSave()}>Save</Button>
+            : <Button onClick={() => this.handleEdit(record.id)}>Edit</Button>
+        }
     }]
   }
 
-  handleEdit = () => {
+  isEditing = record => record.id === this.state.editingKey;
+
+  cancel = () => {
     this.setState({
-      edit: !this.state.edit
+      editingKey: ''
+    });
+  };
+
+  save(form, key) {
+    form.validateFields((error, row) => {
+      if (error) {
+        return;
+      }
+    });
+  }
+
+  // {
+  //     title: 'action',
+  //     dataIndex: 'action',
+  //     className: this.props.login ? "show" : "hide",
+      // render: () => {
+      //   return this.state.edit 
+      //     ? <Button onClick={this.handleSave}>Save</Button>
+      //     : <Button onClick={this.handleEdit}>Edit</Button> 
+      // }
+  //   }
+
+  handleEdit = (key) => {
+    this.setState({
+      edit: !this.state.edit,
+      editingKey: key
     })
   }
 
   handleSave = () => {
     this.setState({
-      edit: !this.state.edit
+      edit: !this.state.edit,
+      editingKey: ''
     })
   }
 
   render() {
-    const { columns } = this.state
+    const components = {
+      body: {
+        row: EditableFormRow,
+        cell: EditableCell,
+      },
+    };
 
-    return <Table rowKey="id" pagination={false} columns={columns} dataSource={this.props.tasks} />
+
+    // const { columns } = this.state
+
+    const columns = this.state.columns.map((col) => {
+      if (!col.editable) {
+        return col;
+      }
+      return {
+        ...col,
+        onCell: record => ({
+          record,
+          inputType: col.dataIndex === 'age' ? 'number' : 'text',
+          dataIndex: col.dataIndex,
+          title: col.title,
+          editing: this.isEditing(record),
+        }),
+      };
+    });
+
+    return <Table 
+      rowKey="id" 
+      components={components}
+      pagination={false} 
+      columns={columns} 
+      dataSource={this.props.tasks} />
   }
 }
 
